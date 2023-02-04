e9d72f95eb3717dc293fe440721d55e3c2021d8d
tachibana97531 <122186925+tachibana97531@users.noreply.github.com>
2023-02-04 12:27:58 +0000

    [add]注文ステータスと製作ステータスの連動

--- app/controllers/admin/order_details_controller.rb
+++ app/controllers/admin/order_details_controller.rb
@@ -1,7 +1,19 @@
 class Admin::OrderDetailsController < ApplicationController
   def update
+    @order = Order.find(params[:order_id])
     @order_detail = OrderDetail.find(params[:id])
-    @order_detail.update(product_params)
+    @order_details = @order.order_details.all
+    
+    is_updated = true
+    if @order_detail.update(product_params)
+      @order.update(status:"making") if @order_detail.production_status == "producting"
+      @order_details.each do |details|
+        if details.production_status != "finished"
+          is_updated = false
+        end
+      end
+      @order.update(status:"priparing") if is_updated
+    end
     redirect_to admin_path(@order_detail.order.id)
   end
   
--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -5,7 +5,10 @@ class Admin::OrdersController < ApplicationController
 
   def update
     @order = Order.find(params[:id])
-    @order.update(status_params)
+    @order_details = OrderDetail.where(order_id:params[:id])
+    if @order.update(status_params)
+      @order_details.update_all(production_status:"wait") if @order.status == "confirm"
+    end
     redirect_to admin_path(@order.id)
   end
   

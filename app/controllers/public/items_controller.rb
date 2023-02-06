3d3fb187020fd1d07f9424f8a6f566c415b24a91
tachibana97531 <122186925+tachibana97531@users.noreply.github.com>
2023-02-06 00:31:48 +0000

    [add]ジャンルごとの商品一覧画面

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -1,9 +1,15 @@
 class Public::ItemsController < ApplicationController
   def index
-    @item_all = Item.all
+    @items = Item.all
+    @genres = Genre.all
+    if params[:genre_id].present?
+      @genre = Genre.find(params[:genre_id])
+      @items = @genre.items
+    end
   end
 
   def show
+    @genres = Genre.all
     @item_find = Item.find(params[:id])
     @cart_item = CartItem.new
   end
